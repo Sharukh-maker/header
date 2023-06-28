6659d4c9115da34d517bab562c4400a6ea21da6e
Sharukh-maker <69448476+Sharukh-maker@users.noreply.github.com>
2023-06-28 11:08:21 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,16 @@
 'use strict';
 
 const build = require('@microsoft/sp-build-web');
-const gulp = require('gulp');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
-const getTasks = build.rig.getTasks;
+var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
-  const result = getTasks.call(build.rig);
+  var result = getTasks.call(build.rig);
 
   result.set('serve', result.get('serve-deprecated'));
 
   return result;
 };
 
-build.initialize(gulp);
-
-gulp.task('gulp-bundle', function() {
-  // Task code goes here
-  // Add your specific task code implementation for the 'gulp-bundle' task
-});
-
-// Additional tasks and configurations can be added below
+build.initialize(require('gulp'));
